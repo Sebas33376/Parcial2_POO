4547dc95eab9b91c342983dc1e4c652b472489e3
Sebas33376 <sebadaniloria0@gmail.com>
2026-06-20 19:48:29 -0300

    arme la estructura básica de la clase cuenta

--- src/parcial2/Cuenta.java
+++ src/parcial2/Cuenta.java
@@ -1,7 +1,75 @@
 package parcial2;
 
-public class Cuenta {
+import java.util.LinkedList;
 
+public class Cuenta {
+	
+	private String nroDeCuenta;
 	private String cbu;
+	private TipoDeCuenta tipoDeCuenta;
+	private double saldo;
+	private int pinCajero;
+	private LinkedList<Movimiento> movimientos;
+	
+	
+	
+	public Cuenta() {
+	}
+
+	public Cuenta(String nroDeCuenta, String cbu, TipoDeCuenta tipoDeCuenta, double saldo, int pinCajero) {
+		setNroDeCuenta(nroDeCuenta);
+		setCbu(cbu);
+		setTipoDeCuenta(tipoDeCuenta);
+		setSaldo(saldo);
+		setPinCajero(pinCajero);
+		this.movimientos = new LinkedList<Movimiento>();
+	}
+	
+	public void AgregarTarjeta() {};
+
+	public String getNroDeCuenta() {
+		return nroDeCuenta;
+	}
+
+	public String getCbu() {
+		return cbu;
+	}
+
+	public TipoDeCuenta getTipoDeCuenta() {
+		return tipoDeCuenta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public int getPinCajero() {
+		return pinCajero;
+	}
+
+	public LinkedList<Movimiento> getMovimientos() {
+		return movimientos;
+	}
+
+	private void setNroDeCuenta(String nroDeCuenta) {
+		this.nroDeCuenta = nroDeCuenta;
+	}
+
+	private void setCbu(String cbu) {
+		this.cbu = cbu;
+	}
+
+	private void setTipoDeCuenta(TipoDeCuenta tipoDeCuenta) {
+		this.tipoDeCuenta = tipoDeCuenta;
+	}
+
+	private void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	private void setPinCajero(int pinCajero) {
+		this.pinCajero = pinCajero;
+	}
+
 	
 }
